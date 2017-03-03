599f15f4fb4b6fdbabe726a5df4c990bef5cc459
Max Gardner <max.c.gardner@gmail.com>
2017-03-02 20:15:46 -0600

    Complete gif generator app

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,45 +2,143 @@
 
 var topics = [
 	{
-		topic: ,
-		search:
+		topic: "lazy",
+		search: "lazy"
 	},
+    {
+        topic: "all the way up",
+        search: "all+the+way+up"
+    },
+    {
+        topic: "i can't even",
+        search: "i+cant+even"
+    },
+    {
+        topic: "wat",
+        search: "wat"
+    },
+    {
+        topic: "i will kill you",
+        search: "i+will+kill+you"
+    },
 	{
-		topic: ,
-		search:
-	}
+		topic: "too high for this shit",
+		search: "too+high+for+this+shit"
+	},
+    {
+        topic: "this bitch",
+        search: "this+bitch"
+    },
+    {
+        topic: "the fuck",
+        search: "the+fuck"
+    },
+    {
+        topic: "suggestive eyebrows",
+        search: "suggestive+eyebrows"
+    },
+    {
+        topic: "uhhh",
+        search: "uhhh"
+    }
 ];
-var queryURL = "http://api.giphy.com/v1/gifs/search?";
 
 // User input variables
 
-// Loop through topics array to display buttons
 
-for (var i = 0; i < topics.length; i++) {
+// Function to call for loop and generate 10 gifs
 
+function makeGifs(gifs, searchTerm) {
+    $("#gif-section").empty();
+
+    for (var i = 0; i < gifs.length; i++) {
+        var still = gifs[i].images.fixed_width_still.url;
+        var animate = gifs[i].images.fixed_width.url;
+        var rating = gifs[i].rating;
+
+        var $newGif = $("<div/>").attr("class", "gif-box well");
+        var $gifElement = $("<img>").attr({"src": still, "data-animate": animate, "data-still": still, "class": "giphy", "alt": searchTerm});
+        $newGif.html('<p class="label label-warning">Rating: ' + rating + "</p><br><br>" + $gifElement[0].outerHTML);
+        $("#gif-section").append($newGif);
+    }
 }
 
-// API Key: dc6zaTOxFJmzC
-// http://api.giphy.com/v1/gifs/search?
-// -- search term -- q=search+term+here
-// -- limit -- &limit=10
+// Loop through topics array to display buttons
+
+function showButtons() {
+
+    if ($("#button-section").children().length > 0) {
+        var $button = $("<button/>").attr({"data-topic": topics[topics.length - 1].search, "class": "search-term btn btn-default"}).html(topics[topics.length - 1].topic);
+        $("#button-section").append($button);
+    } else {
+        for (var i = 0; i < topics.length; i++) {
+            var $button = $("<button/>").attr({"data-topic": topics[i].search, "class": "search-term btn btn-default"}).html(topics[i].topic);
+            $("#button-section").append($button);
+        }
+    }
+}
+
+showButtons();
+
+// Query the API when someone clicks a button or adds a button
+
+function queryAPI(searchTerm) {
+    var apiKey = "&api_key=dc6zaTOxFJmzC";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?";
+    queryURL += "q=" + searchTerm + "&limit=10" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(result) {
+        var gifs = result.data;
+        makeGifs(gifs, searchTerm);
+    }).fail(function(error) {
+        console.log(error);
+        alert("Something went wrong. Try your request again!");
+    });
+}
 
 // When someone clicks on button, pull search term and query Giphy API to pull 10 corresponding GIFs
 
 $(document).on("click", ".search-term", function() {
+    var searchTerm = $(this).data("topic");
+    queryAPI(searchTerm);
+});
 
-var searchTerm = $(this).data("topic");
+// When someone clicks on still GIF image, animate it
 
-queryURL += searchTerm + "&limit=10";
+$(document).on("click", "img.giphy", function() {
+    var stillData = $(this).data("still");
+    var animateData = $(this).data("animate");
+    var currentSrc = $(this).attr("src");
 
-$.ajax({
-	url: queryURL,
-	method: "GET"
-}).done(function(result) {
-	var gif = result.data;
-	console.log(gif);
-}).fail(function(error) {
-	console.log(error);
-	alert("Something went wrong. Try your request again!");
-})
+    if (currentSrc === stillData) {
+        $(this).attr("src", animateData);
+    } else {
+        $(this).attr("src", stillData);
+    }
+});
+
+$("#add-button").on("click", function(event) {
+    event.preventDefault();
+    var newTerm = $("#new-search").val().trim();
+    $("#new-search").val("");
+    
+    function parseSearch() {
+        var str = newTerm
+        str = str.replace(/'/g, '');
+        var parsedTerm = str.split(" ");
+        return parsedTerm.join("+");
+    }
+
+    var parsedTerm = parseSearch();
+    var newSearch = {
+        topic: newTerm,
+        search: parsedTerm
+    };
+
+    topics.push(newSearch);
+    showButtons();
+    queryAPI(parsedTerm);
 });
